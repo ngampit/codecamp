4e4e0b7e47bb2f6b7f7f55a4c3481c07bc954b2f
ngampit <ngampitt@yahoo.com>
2020-11-04 22:20:29 -0800

    callbackLab1

---
+++ callbackLab1.js
@@ -0,0 +1,14 @@
+//Callback Lab-1
+
+setTimeout(() => {
+    console.log('a')
+}, 50)
+setTimeout(() => {
+    console.log('b')
+}, 1050)  
+setTimeout(() => {
+    console.log('c')
+}, 2050)  
+setTimeout(() => {
+    console.log('d')
+}, 3050) 
